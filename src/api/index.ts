3feb42d3d22535553ce81c868da1fd8645425e44
ArwaAlattas <arwanabeelalattas@gmail.com>
2024-05-28 05:54:31 +0300

    Edit baseUrl

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { config } from 'process'
 
 const isDevelopment = import.meta.env.MODE === 'development'
-let baseURL = 'http://localhost:5125/api'
+let baseURL = 'https://e-commerce-application-api-nxd1.onrender.com/api'
 
 if (!isDevelopment) {
   // Update this later when you have a working backend server
